f8dbb2022994fdbaab6e14332a17850411ae6ac8
1979dany <danymail1979@gmail.com>
2022-06-13 13:51:50 -0700

    Write a function that swaps the values of two integers.

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,14 +7,13 @@
  *
  * Return: pointer to dest.
  */
-
 char *_strcpy(char *dest, char *src)
 {
 	int j;
 	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[j] = src[j];
-	}
-		dest[j] = '\0';
-		return (dest);
+		{
+			dest[j] = src[j];
+		}
+	dest[j] = '\0';
+	return (dest)
 }
